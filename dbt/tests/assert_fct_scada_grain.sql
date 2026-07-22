cec1c170401512c0fcde489ad7a1f75eee121372
djouallah <12554469+djouallah@users.noreply.github.com>
2026-07-22 16:52:58 +1000

    Revert "test(dbt): scope grain tests to the merge-strategy cutover date"
    This reverts commit dafd0c191114240469cf9f1feef710c357ddc932.

--- dbt/tests/assert_fct_scada_grain.sql
+++ dbt/tests/assert_fct_scada_grain.sql
@@ -1,10 +1,7 @@
 -- Grain check: the merge key of fct_scada must be unique.
--- Scoped to the 2026-07-23 merge-strategy cutover (see the _today grain tests for
--- the legacy-duplicate story).
 {{ config(tags=['heavy']) }}
 
 SELECT file, DUID, SETTLEMENTDATE, INTERVENTION, COUNT(*) AS n
 FROM {{ ref('fct_scada') }}
-WHERE DATE >= DATE '2026-07-23'
 GROUP BY ALL
 HAVING COUNT(*) > 1
